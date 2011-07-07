e04160c3c53d7018f15df1d6caa32af6d3ffbf31
wokai <wokai@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-07-07 13:49:59 +0000

    import in wrong place
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rbamtools@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src/bamtools/BamWriter.d
+++
@@ -1,8 +0,0 @@
-bamtools/BamWriter.d bamtools/BamWriter.o: bamtools/BamWriter.cpp \
- bamtools/BGZF.h bamtools/BamWriter.h bamtools/BamAux.h
-
-bamtools/BGZF.h:
-
-bamtools/BamWriter.h:
-
-bamtools/BamAux.h:
