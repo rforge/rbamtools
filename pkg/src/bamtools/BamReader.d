713f98184bf0d4b191cbfd7c6587bf97fd5a7622
wokai <wokai@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-02-07 11:10:51 +0000

    deletion of pkg as preparation for import of version 2.0
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rbamtools@29 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/src/bamtools/BamReader.d
+++
@@ -1,8 +0,0 @@
-bamtools/BamReader.d bamtools/BamReader.o: bamtools/BamReader.cpp \
- bamtools/BGZF.h bamtools/BamReader.h bamtools/BamAux.h
-
-bamtools/BGZF.h:
-
-bamtools/BamReader.h:
-
-bamtools/BamAux.h:
