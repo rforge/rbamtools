bcf17e0f91ce0cdc850d19028d426ac2a53ba42e
wokai <wokai@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-06-05 09:43:54 +0000

    Initial import of rdef.h
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rbamtools@72 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/src/samtools/rdef.h
@@ -0,0 +1,61 @@
+/*
+ * rdef.h
+ *
+ *  Created on: 30.05.2014
+ *      Author: wolfgang
+ */
+
+#ifndef RDEF_H_
+#define RDEF_H_
+
+
+/* Turn R definition on and off */
+#define R_CRAN
+
+
+
+#ifdef R_CRAN
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Library is compiled under R
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+#include <R.h>
+
+#else
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *  Library is compiled without R
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+#define R_INLINE inline
+
+#define Rprintf printf
+/*
+ * Variadic macros
+ * https://gcc.gnu.org/onlinedocs/cpp/Variadic-Macros.html
+ */
+#define error(...)	\
+	printf(__VA_ARGS__);	\
+	exit (EXIT_FAILURE);
+#endif /* R_CRAN */
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Library is compiled under C99 standard
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+#if __STDC_VERSION__==199901L
+
+/* fileno removed (only used for connection to stdin and stdout */
+//#define _POSIX_C_SOURCE 200112L /* Used for c99 definition of fileno */
+/* Replaced strdup calls by 3-line implementations */
+//#define _SVID_SOURCE            /* Used for c99 definition of strdup */
+
+/*
+ * See:
+ * http://www.gnu.org/software/libc/manual/html_node/File-Positioning.html
+ * The ftello function is similar to ftell, except that it returns a value of type off_t.
+ * Replaced ftello in order to become c99 compliant
+ */
+#define ftello(x) ftell(x)
+
+#endif  /* __STDC_VERSION__ */
+#endif /* RDEF_H_           */
