59300ce3f918bcafd6ec1bd93492f8316d323d0d
wokai <wokai@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-07-14 12:02:20 +0000

    Initial import of new test directory
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rbamtools@86 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/tests/test-all.R
@@ -0,0 +1,41 @@
+
+
+## + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + ##
+## Load prerequisites
+## + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + ##
+
+require(rbamtools)
+
+
+## + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + ##
+## Initialize example data
+## + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + ##
+
+bam<- system.file("extdata", "accepted_hits.bam", package="rbamtools")
+idx<- system.file("extdata", "accepted_hits.bam.bai", package="rbamtools")
+# Open BAM-file for reading
+reader<-bamReader(bam,idx=TRUE)
+
+
+## + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + ##
+## Run tests
+## + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + ##
+source("test_bam_range.r")
+
+
+
+
+## + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + ##
+## Cleanup
+## + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + ##
+
+bamClose(reader)
+rm(reader)
+gc()
+
+cat("[rbamtools] rest-all.R tests finished.\n")
+
+
+## + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + ##
+## END OF FILE
+## + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + + ##
